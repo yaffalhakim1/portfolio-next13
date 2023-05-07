077e394f012df2c48e85745d21a9f2d5adfecfa3
yaffalhakim1 <yafi.tamfan08@gmail.com>
2023-05-07 09:39:00 +0700

    add animation on intro

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import Sidebar from "@/components/sidebar";
+import { motion } from "framer-motion";
 import "./globals.css";
 import clsx from "clsx";
 import { Providers } from "./providers";
 import { useEffect, useState } from "react";
 import { Plus_Jakarta_Sans } from "next/font/google";
-
-// export const metadata = {
-//   title: "Muhammad Yafi Alhakim",
-//   description:
-//     "Hi there! I&apos;m a final year student with a passion for web development",
-// };
 
 const jakarta = Plus_Jakarta_Sans({
   weight: ["400", "700"],
@@ -31,52 +26,59 @@ export default function RootLayout({
     }, 3000);
     return () => clearTimeout(timer);
   }, []);
+
   return (
     <>
       <html lang="en" suppressHydrationWarning>
         <body>
           <Providers>
+            <motion.div
+              initial={{ opacity: showPage ? 0 : 1 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0, transition: { duration: 1 } }}
+              transition={{ duration: 1 }}
+              className="flex flex-col items-center justify-center w-full h-full fixed top-0 left-0 z-50"
+            >
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { duration: 1 } }}
+                transition={{ duration: 1 }}
+                className="flex flex-col items-center justify-center"
+              >
+                {showPage && (
+                  <motion.p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0, transition: { duration: 1 } }}
+                    transition={{ duration: 1 }}
+                    className="text-2xl font-bold"
+                  >
+                    Hire Yafi he is good with frontend things!
+                  </motion.p>
+                )}
+                {showPage && (
+                  <motion.p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0, transition: { duration: 1 } }}
+                    transition={{ duration: 1 }}
+                    className="text-md mt-3"
+                  >
+                    - some friends
+                  </motion.p>
+                )}
+              </motion.div>
+            </motion.div>
             <div
+              style={{ display: showPage ? "none" : "flex" }}
               className={clsx(
                 "flex flex-col md:flex-row lg:w-[1000px] mx-4 lg:mx-auto lg:mt-[100px] mt-8 antialiased max-w-4xl mb-40",
                 jakarta.className
               )}
             >
-              {showPage ? (
-                <div
-                  className={`flex flex-col items-center justify-center w-full h-full fixed top-0 left-0 z-50 ${
-                    showPage
-                      ? "opacity-100 transition-opacity duration-1000"
-                      : "opacity-0"
-                  }`}
-                >
-                  <div className="flex flex-col items-center justify-center">
-                    <p
-                      className={`text-2xl font-bold ${
-                        showPage
-                          ? "opacity-100 transition-opacity duration-1000"
-                          : "opacity-0"
-                      }`}
-                    >
-                      Hire him he is so good with react things!
-                    </p>
-                    <p
-                      className={`text-md mt-4 ${
-                        showPage
-                          ? "opacity-100 transition-opacity duration-1000"
-                          : "opacity-0"
-                      }`}
-                    >
-                      - some friends
-                    </p>
-                  </div>
-                </div>
-              ) : (
-                <>
-                  <Sidebar />
-                  <main className="flex flex-grow w-full">{children}</main>
-                </>
-              )}
+              <Sidebar />
+              <main>{children}</main>
             </div>
           </Providers>
         </body>
